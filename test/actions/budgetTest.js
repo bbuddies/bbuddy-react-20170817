cc06f5f420c2e2954cb1cccd0664e6dee6d24310
蒋凯 <baidan4855@qq.com>
2017-08-18 15:14:19 +0800

    unit test for modify action

--- test/actions/budgetTest.js
+++ test/actions/budgetTest.js
@@ -1,22 +1,19 @@
-import {addBudget, loadBudgets} from "../../app/actions/budget";
+import {addBudget, loadBudgets, modifyBudget} from "../../app/actions/budget";
 import * as actions from "../../app/actions/budget.generated";
 
 describe('budget action', () =>{
-  let dispatch, getState, budget
+  let dispatch, getState, budget, success
   beforeEach(() => {
     dispatch = sinon.stub().returns(Promise.resolve({}))
     getState = null
     budget = {}
+    success = sinon.spy()
   })
   it('load budgets', () => {
     loadBudgets()(dispatch, getState)
     dispatch.should.be.calledWith(actions.fetchBudgets())
   })
   context('add budget', () => {
-    let success
-    beforeEach(() => {
-      success = sinon.spy()
-    })
     it('dispatch create budget', () => {
       addBudget(budget, success)(dispatch, getState)
       dispatch.should.be.calledWith(actions.createBudget(budget))
@@ -29,6 +26,22 @@ describe('budget action', () =>{
     it('no callback when add fail', () => {
       dispatch.returns(Promise.resolve({type: actions.ADD_BUDGET_FAILURE}))
       addBudget(budget, success)(dispatch, getState)
+      success.should.not.be.called
+    })
+  })
+  context('modify budget', () => {
+    it('dispatch modify budget', () => {
+      modifyBudget(budget, success)(dispatch, getState)
+      dispatch.should.be.calledWith(actions.updateBudget(budget))
+    })
+    it('callback when modify success', () => {
+      dispatch.returns(Promise.resolve({type: actions.UPDATE_BUDGET_SUCCESS}))
+      modifyBudget(budget, success)(dispatch, getState)
+      success.should.be.called
+    })
+    it('no callback when modify fail', () => {
+      dispatch.returns(Promise.resolve({type: actions.UPDATE_BUDGET_FAILURE}))
+      modifyBudget(budget, success)(dispatch, getState)
       success.should.not.be.called
     })
   })
