7e773006f0d90806db7876c66e6990d865cce311
Jackson Zhang <zbcjackson@gmail.com>
2017-08-19 17:11:19 +0800

    TDD query budgets

--- app/presenters/budgetsPagePresenter.js
+++ app/presenters/budgetsPagePresenter.js
@@ -5,9 +5,10 @@ import values from 'lodash/values'
 import moment from 'moment'
 import * as BudgetActions from '../actions/budget'
 import * as NavigationActions from '../actions/navigation'
+import _ from 'lodash'
 
 export class BudgetsPagePresenter {
-  constructor(props){
+  constructor(props) {
     this.props = props
   }
 
@@ -19,41 +20,58 @@ export class BudgetsPagePresenter {
     }
   }
 
-  loadData(){
+  loadData() {
     this.props.loadBudgets()
   }
 
-  getParamForTotal(budget){
+  getParamForTotal(budget) {
     const daysOfBudget = moment(budget.month).daysInMonth()
     const oneDayBudget = budget.amount / daysOfBudget
-    return { daysOfBudget, oneDayBudget }
+    return {daysOfBudget, oneDayBudget}
   }
 
   calcBudgets({startAt, endAt}) {
     let total = 0
     const startDay = moment(startAt).get('date')
     const endDay = moment(endAt).get('date')
-    const { budgets } = this.props
+    const {budgets} = this.props
     budgets && budgets.forEach(budget => {
-      if (moment(startAt).isSame(moment(budget.month),'year') && moment(startAt).isSame(moment(budget.month),'month')) {
-        const { daysOfBudget, oneDayBudget } = this.getParamForTotal(budget)
+      if (moment(startAt).isSame(moment(budget.month), 'year') && moment(startAt).isSame(moment(budget.month), 'month')) {
+        const {daysOfBudget, oneDayBudget} = this.getParamForTotal(budget)
         let days = daysOfBudget
-        if(moment(startAt).isSame(moment(endAt),'month')) {
+        if (moment(startAt).isSame(moment(endAt), 'month')) {
           days = endDay
         }
         total += oneDayBudget * (days - startDay + 1)
       }
-      if (moment(endAt).isSame(moment(budget.month),'year')
-          && moment(endAt).isSame(moment(budget.month),'month')
-          && !moment(startAt).isSame(moment(endAt),'month')) {
-        const { daysOfBudget, oneDayBudget } = this.getParamForTotal(budget)
+      if (moment(endAt).isSame(moment(budget.month), 'year')
+        && moment(endAt).isSame(moment(budget.month), 'month')
+        && !moment(startAt).isSame(moment(endAt), 'month')) {
+        const {daysOfBudget, oneDayBudget} = this.getParamForTotal(budget)
         total += oneDayBudget * endDay
       }
-      if (moment(budget.month).isBetween(startAt, endAt,'month')){
+      if (moment(budget.month).isBetween(startAt, endAt, 'month')) {
         total += budget.amount
       }
     })
     return total.toFixed(2)
+  }
+
+  query({start, end}) {
+    let startDate = moment(start, 'YYYY-MM-DD')
+    let endDate = moment(end, 'YYYY-MM-DD')
+
+    let budgetMonth = budget => moment(budget.month, 'YYYY-MM');
+    let startOfBudget = budget => budgetMonth(budget).startOf('month')
+    let endOfBudget = budget => budgetMonth(budget).endOf('month')
+    let queryOverlappingAmount = (budget, {startDate, endDate}) => {
+      const overlappingStart = startDate.isAfter(startOfBudget(budget)) ? startDate : startOfBudget(budget)
+      const overlappingEnd = endDate.isBefore(endOfBudget(budget)) ? endDate : endOfBudget(budget);
+      return budget.amount / budgetMonth(budget).daysInMonth() * dayCountBetween({start: overlappingStart, end: overlappingEnd})
+    }
+    let dayCountBetween = ({start, end}) => start.isAfter(end)? 0 : (end.diff(start, 'days') + 1)
+
+    return _.sumBy(this.props.budgets, budget => queryOverlappingAmount(budget, {startDate, endDate}))
   }
 
   static mapStateToProps(state) {
--- test/presenters/budgetPagePresenterTest.js
+++ test/presenters/budgetPagePresenterTest.js
@@ -1,47 +1,105 @@
-import { BudgetsPagePresenter} from '../../app/presenters/budgetsPagePresenter'
+import {BudgetsPagePresenter} from '../../app/presenters/budgetsPagePresenter'
 
 describe('BudgetPagePresenter', () => {
-    context('Calc Total Amount', () => {
-        let presenter
-        beforeEach(() => {
-          let props = {
-              budgets: [
-                { month: '2016-10', amount: 3100 },
-                { month: '2016-08', amount: 3100 },
-                { month: '2016-05', amount: 3100 },
-                { month: '2016-11', amount: 3000 },
-                { month: '2017-01', amount: 3100 },
-                { month: '2008-02', amount: 2900 },
-                { month: '2009-02', amount: 2900 },
-              ]}
-          presenter = new BudgetsPagePresenter(props)
-        })
-        it('budgets between 2016-09-01 and 2016-10-02 should be 200.00', () => {
-            presenter.calcBudgets({startAt: '2016-09-01', endAt: '2016-10-02'}).should.be.equal('200.00')
-        })
-        it('budgets between 2016-10-01 and 2016-11-15 should be 4600.00', () => {
-            presenter.calcBudgets({startAt: '2016-09-01', endAt: '2016-11-15'}).should.be.equal('4600.00')
-        })
-        it('budgets between 2017-02-01 and 2017-11-15 should be 0.00', () => {
-            presenter.calcBudgets({startAt: '2017-02-01', endAt: '2017-11-15'}).should.be.equal('0.00')
-        })
-        it('budgets between 2008-02-01 and 2008-02-15 should be 1500.00', () => {
-            presenter.calcBudgets({startAt: '2008-02-01', endAt: '2008-02-15'}).should.be.equal('1500.00')
-        })
-        it('budgets between 2009-02-01 and 2009-02-15 should be 1553.57', () => {
-            presenter.calcBudgets({startAt: '2009-02-01', endAt: '2009-02-15'}).should.be.equal('1553.57')
-        })
-        it('budgets between 2016-05-01 and 2016-10-15 should be 7700.00', () => {
-            presenter.calcBudgets({startAt: '2016-05-01', endAt: '2016-10-15'}).should.be.equal('7700.00')
-        })
-        it('budgets between 2008-01-01 and 2008-02-01 should be 100.00', () => {
-            presenter.calcBudgets({startAt: '2008-01-01', endAt: '2008-02-01'}).should.be.equal('100.00')
-        })
-        it('budgets between 2008-02-01 and 2008-02-01 should be 100.00', () => {
-            presenter.calcBudgets({startAt: '2008-02-01', endAt: '2008-02-01'}).should.be.equal('100.00')
-        })
-        it('budgets between 2008-02-01 and 2016-05-01 should be 5900.00', () => {
-            presenter.calcBudgets({startAt: '2008-02-01', endAt: '2016-05-01'}).should.be.equal('5900.00')
-        })
+  context('Calc Total Amount', () => {
+    let presenter
+    beforeEach(() => {
+      let props = {
+        budgets: [
+          {month: '2016-10', amount: 3100},
+          {month: '2016-08', amount: 3100},
+          {month: '2016-05', amount: 3100},
+          {month: '2016-11', amount: 3000},
+          {month: '2017-01', amount: 3100},
+          {month: '2008-02', amount: 2900},
+          {month: '2009-02', amount: 2900},
+        ]
+      }
+      presenter = new BudgetsPagePresenter(props)
     })
+    it('budgets between 2016-09-01 and 2016-10-02 should be 200.00', () => {
+      presenter.calcBudgets({startAt: '2016-09-01', endAt: '2016-10-02'}).should.be.equal('200.00')
+    })
+    it('budgets between 2016-10-01 and 2016-11-15 should be 4600.00', () => {
+      presenter.calcBudgets({startAt: '2016-09-01', endAt: '2016-11-15'}).should.be.equal('4600.00')
+    })
+    it('budgets between 2017-02-01 and 2017-11-15 should be 0.00', () => {
+      presenter.calcBudgets({startAt: '2017-02-01', endAt: '2017-11-15'}).should.be.equal('0.00')
+    })
+    it('budgets between 2008-02-01 and 2008-02-15 should be 1500.00', () => {
+      presenter.calcBudgets({startAt: '2008-02-01', endAt: '2008-02-15'}).should.be.equal('1500.00')
+    })
+    it('budgets between 2009-02-01 and 2009-02-15 should be 1553.57', () => {
+      presenter.calcBudgets({startAt: '2009-02-01', endAt: '2009-02-15'}).should.be.equal('1553.57')
+    })
+    it('budgets between 2016-05-01 and 2016-10-15 should be 7700.00', () => {
+      presenter.calcBudgets({startAt: '2016-05-01', endAt: '2016-10-15'}).should.be.equal('7700.00')
+    })
+    it('budgets between 2008-01-01 and 2008-02-01 should be 100.00', () => {
+      presenter.calcBudgets({startAt: '2008-01-01', endAt: '2008-02-01'}).should.be.equal('100.00')
+    })
+    it('budgets between 2008-02-01 and 2008-02-01 should be 100.00', () => {
+      presenter.calcBudgets({startAt: '2008-02-01', endAt: '2008-02-01'}).should.be.equal('100.00')
+    })
+    it('budgets between 2008-02-01 and 2016-05-01 should be 5900.00', () => {
+      presenter.calcBudgets({startAt: '2008-02-01', endAt: '2016-05-01'}).should.be.equal('5900.00')
+    })
+  })
+
+  context('query budgets', () => {
+    let presenter
+    let givenBudgets = budgets => presenter = new BudgetsPagePresenter({budgets})
+    it('no budgets', () => {
+      givenBudgets([])
+
+      presenter.query({start: '2017-07-01', end: '2017-08-01'}).should.be.equal(0)
+    })
+    it('query one month', () => {
+      givenBudgets([{month: '2017-07', amount: 3100}])
+
+      presenter.query({start: '2017-07-01', end: '2017-07-31'}).should.be.equal(3100)
+    })
+    it('query one day in a budget', () => {
+      givenBudgets([{month: '2017-07', amount: 3100}])
+
+      presenter.query({start: '2017-07-01', end: '2017-07-01'}).should.be.equal(100)
+    })
+    it('query two days in a budget', () => {
+      givenBudgets([{month: '2017-07', amount: 3100}])
+
+      presenter.query({start: '2017-07-01', end: '2017-07-02'}).should.be.equal(200)
+    })
+    it('query start day before budget', () => {
+      givenBudgets([{month: '2017-07', amount: 3100}])
+
+      presenter.query({start: '2017-06-30', end: '2017-07-03'}).should.be.equal(300)
+    })
+    it('query end day after budget', () => {
+      givenBudgets([{month: '2017-07', amount: 3100}])
+
+      presenter.query({start: '2017-07-30', end: '2017-08-03'}).should.be.equal(200)
+    })
+    it('query out of a budget', () => {
+      givenBudgets([{month: '2017-07', amount: 3100}])
+
+      presenter.query({start: '2017-08-01', end: '2017-08-03'}).should.be.equal(0)
+    })
+    it('query across two budgets', () => {
+      givenBudgets([
+        {month: '2017-07', amount: 3100},
+        {month: '2017-08', amount: 3100},
+      ])
+
+      presenter.query({start: '2017-07-20', end: '2017-08-03'}).should.be.equal(1500)
+    })
+    it('query budgets with various amount', () => {
+      givenBudgets([
+        {month: '2017-07', amount: 3100},
+        {month: '2017-08', amount: 310},
+      ])
+
+      presenter.query({start: '2017-07-20', end: '2017-08-03'}).should.be.equal(1230)
+    })
+  })
+
 })
